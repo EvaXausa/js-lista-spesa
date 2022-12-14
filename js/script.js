00932a3b3c0ccc422c6332f0fbc0e46c910afaf9
EvaXausa <xausa.eva@gmail.com>
2022-12-14 18:30:33 +0100

    list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 /*
 1- Inventare una lista della spesa 
-2- Stampare in pagina gli elementi della lista individualmente
+2- Stampo in console la lista
+3- Stampare in pagina gli elementi della lista individualmente
 */
+
+//Inventare una lista della spesa
+const list = ["pane", "latte", "uova", "zucchero"];
+
+//Stampo in console la lista
+let i = 0;
+while (i < list.length) {
+  console.log(list[i]);
+  i++;
+}
